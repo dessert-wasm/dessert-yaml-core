7e069a951c2317990b82055ed388adc64dcc5c45
Nyquase <adrien.rouhete@epitech.eu>
2019-05-18 17:46:28 +0200

    loadSafe now throws when failing

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@ mod utils;
 use wasm_bindgen::prelude::*;
 
 extern crate serde_json;
+use serde_json::json;
+use serde_json::Value;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -9,11 +11,35 @@ extern crate serde_json;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-#[wasm_bindgen]
-pub fn load(yml_str: &str) -> JsValue {
-    let yml: serde_json::Value = match serde_yaml::from_str(&yml_str) {
+#[wasm_bindgen(js_name = safeLoad)]
+pub fn safe_load(yml_str: &str) -> Result<JsValue, JsValue> {
+    let yml: Value = match serde_yaml::from_str(&yml_str) {
         Ok(res) => res,
-        Err(_) => { return JsValue::from_serde("x : 1").unwrap(); }
+        Err(e) => { 
+            let location = e.location();
+            return Err(
+                JsValue::from_serde(&json!({
+                    "mark": {
+                        "buffer": yml_str.to_string(),
+                        "name": Value::Null,
+                        "column": match &location { 
+                            Some(l) => Value::Number(l.column().into()),
+                            None => Value::Null,
+                        },
+                        "line": match &location { 
+                            Some(l) => Value::Number(l.line().into()),
+                            None => Value::Null,
+                        },
+                        "position": match &location { 
+                            Some(l) => Value::Number(l.index().into()),
+                            None => Value::Null,
+                        },
+                    },
+                    "message": e.to_string(),
+                    "name": "YAMLException"
+                }))
+            .unwrap());
+        }
     };
-    JsValue::from_serde(&yml).unwrap()
+    Ok(JsValue::from_serde(&yml).unwrap())
 }
